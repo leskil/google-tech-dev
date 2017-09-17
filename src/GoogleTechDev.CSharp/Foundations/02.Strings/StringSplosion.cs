e88cc724953d46dadc603aea4c11ce3fb29184cb
Lasse Eskildsen <les@vertica.dk>
2017-09-17 23:01:44 +0200

    StringSplotion

---
+++ src/GoogleTechDev.CSharp/Foundations/02.Strings/StringSplosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace GoogleTechDev.CSharp.Foundations._02.Strings
+{
+    [TestFixture]
+    public class StringSplosion : TestClass
+    {
+        [Test]
+        [TestCase("ab", "aab")]
+        [TestCase("abc", "aababc")]
+        [TestCase("Code", "CCoCodCode")]
+        public void ForLoop(string input, string expectedResult)
+        {
+            var result = string.Empty;
+
+            for (var i =0; i < input.Length; i++)
+            {
+                result = result + input.Substring(0, i + 1);
+            }
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
